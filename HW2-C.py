297eed75a7fdc34e3f6c3a27055ff77b8f360b34
LIN,YU-HAO <s1142446@pu.edu.tw>
2025-09-30 11:13:39 +0000

    0930

--- HW2-C.py
+++ HW2-C.py
@@ -1,4 +1,5 @@
 #C
+'''
 import math
 
 def c_diagonal(length, width):
@@ -9,3 +10,9 @@ length, width = map(int, input().split())
 
 d_length = c_diagonal(length, width)
 print(f"{d_length:.2f}")
+'''
+
+import math
+l,w = map(int, input().split())
+d = float(math.sqrt(l**2+w**2))
+print(f"{d:.2f}")
